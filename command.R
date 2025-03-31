762f6fe78ed7e998bfcabcc42f214f103f1b02ab
ngmthu234 <ngmthu.234@gmail.com>
2025-03-31 14:55:23 -0400

    data cleanup #2
    group_by(State, County) to create a grouping structure based on combinations of values in columns State and County, preparing for summarize().
    
    summarize() to summarize statistics --is used in conjunction with group_by() to summarize statistics based state-county combination. 
    
    "Inventory (Animal heads)" = sum(Value) to create column "Inventory (Animal heads)" and assign the new column the sum of column Value within each state-county combination.

--- command.R
+++ command.R
@@ -2,3 +2,5 @@ beef_2022 <- read.csv("~/Documents/BSE project/cow_inventory_beef_2022.csv")
 
 library(tidyverse)
 beef_2022 <- beef_2022 %>% dplyr::filter(Value != " (D)") %>% mutate(Value = gsub(",", "", Value)) %>% mutate(Value = as.numeric(Value))
+
+beef_2022_bycounty <- beef_2022 %>% group_by(State, County) %>% summarize("Inventory (Animal heads)" = sum(Value))
