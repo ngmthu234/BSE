f5291e0d8d1fcabcbcddaf74613dbe04f63a125f
ngmthu234 <ngmthu.234@gmail.com>
2025-03-31 14:27:16 -0400

    csv cleanup #1
    library(tidyverse) to load tidyverse for data manipulation.
    
    dplyr::filter(Value != " (D)") to remove the string " (D)" in column Value.
    
    mutate(Value = gsub(",", "", Value) to globally substitute "," to an empty string in column Value, preparing for numerical conversion.
    
    mutate (Value = as.numeric(Value)) to convert a string in column Value into a number, allowing arithmetic calculations.

--- command.R
+++ command.R
@@ -1,1 +1,4 @@
 beef_2022 <- read.csv("~/Documents/BSE project/cow_inventory_beef_2022.csv")
+
+library(tidyverse)
+beef_2022 <- beef_2022 %>% dplyr::filter(Value != " (D)") %>% mutate(Value = gsub(",", "", Value)) %>% mutate(Value = as.numeric(Value))
