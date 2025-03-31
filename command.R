12f2f80a767e3f77c6f0edcfa9b6b19d1ae44da4
ngmthu234 <ngmthu.234@gmail.com>
2025-03-31 15:08:44 -0400

    load libraries for spatial data
    tigris to download spatial data. 
    
    sf to manipulate spatial data. 
    
    rmapshaper to simplify spatial data (optional).

--- command.R
+++ command.R
@@ -4,3 +4,7 @@ library(tidyverse)
 beef_2022 <- beef_2022 %>% dplyr::filter(Value != " (D)") %>% mutate(Value = gsub(",", "", Value)) %>% mutate(Value = as.numeric(Value))
 
 beef_2022_bycounty <- beef_2022 %>% group_by(State, County) %>% summarize("Inventory (Animal heads)" = sum(Value))
+
+library(tigris)
+library(sf)
+library(rmapshaper)
