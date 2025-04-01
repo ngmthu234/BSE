f5fae8764cafbf775e40c76a61b002ef6bde4717
ngmthu234 <ngmthu.234@gmail.com>
2025-04-01 17:27:42 -0400

    plot heatmap
    ggplot() is to plot object.
    
    geom_sf() is to plot spatial data. 
    
    data = is to specify the data source. 
    
    aes(fill = `Inventory (Animal heads)`) 
    - aes() alllows ggplot() to fill colors of each counties based on values of column Inventory (Animal heads). scale_fill_gradient() then determines how the values are translated into colors. 
    - aes() is not used in the second geom_sf() because it is not a data-driven fill; hence, fill = NA as we only want to display the outlines of the states. 
    - backticks (``) are used instead of double quotes ("") to tell ggplot2 that Inventory (Animal heads) is a column name, not a string. 
    
    color = is to set the county/state outline color. 
    
    size = is to set the county/state oultline thickness. 
    
    coord_sf() is to set the coordinate reference system of the output plot. 
    - The set CRS is Albers Equal Area Conic projection.
    
    labs() is to set plot labels.
    
    them_void() is to remove unnecessary plot elements like gridlines.

--- command.R
+++ command.R
@@ -9,7 +9,7 @@ library(tigris)
 library(sf)
 library(rmapshaper)
 
-us_counties <- counties(cb = TRUE, resolution = "20")
+us_counties <- counties(cb = TRUE, resolution = "20m")
 us_states <- states(cb = TRUE, resolution = "5m")
 
 beef_2022_bycounty <- beef_2022_bycounty %>% dplyr::filter(State != "ALASKA") %>% dplyr::filter(State != "HAWAII")
@@ -22,3 +22,5 @@ us_counties$NAME <-tolower(us_counties$NAME)
 us_counties$STATE_NAME <-tolower(us_counties$STATE_NAME)
 
 beef_2022_map <- merge(us_counties, beef_2022_bycounty, by.x = c("STATE_NAME", "NAME"), by.y = c("State", "County"), all.x = TRUE) %>% st_as_sf()
+
+ggplot() + geom_sf(data = beef_2022_map, aes(fill = `Inventory (Animal heads)`), color = "gray30", size = 1) + geom_sf(data = us_states, fill = NA, color = "black", size = 500) + coord_sf(crs = st_crs ("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96")) + scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") + labs(title = "Beef cattle population by U.S. county", fill = "Inventory (Animal heads)") + theme_void()
