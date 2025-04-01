b835eeb13c7614ecae688625c32beb4b189bf13d
ngmthu234 <ngmthu.234@gmail.com>
2025-03-31 21:22:55 -0400

    fix error #1
    missing ":" in dply::filter in lines 16 and 17. 
    
    delete colum name changes for beef_2022_map.

--- command.R
+++ command.R
@@ -13,8 +13,8 @@ us_counties <- counties(cb = TRUE, resolution = "5m")
 us_states <- states(cb = TRUE, resolution = "500k")
 
 beef_2022_bycounty <- beef_2022_bycounty %>% dplyr::filter(State != "ALASKA") %>% dplyr::filter(State != "HAWAII")
-us_counties <- us_counties %>% dplyr:filter(STUSPS != "AK") %>% dplyr:filter(STUSPS != "AS") %>% dplyr:filter(STUSPS != "DC") %>% dplyr:filter(STUSPS != "GU") %>% dplyr:filter(STUSPS != "HI") %>% dplyr:filter(STUSPS != "MP") %>% dplyr:filter(STUSPS != "PR") %>% dplyr:filter(STUSPS != "VI")
-us_states <- us_states %>% dplyr:filter(STUSPS != "AK") %>% dplyr:filter(STUSPS != "AS") %>% dplyr:filter(STUSPS != "DC") %>% dplyr:filter(STUSPS != "GU") %>% dplyr:filter(STUSPS != "HI") %>% dplyr:filter(STUSPS != "MP") %>% dplyr:filter(STUSPS != "PR") %>% dplyr:filter(STUSPS != "VI")
+us_counties <- us_counties %>% dplyr::filter(STUSPS != "AK") %>% dplyr::filter(STUSPS != "AS") %>% dplyr::filter(STUSPS != "DC") %>% dplyr::filter(STUSPS != "GU") %>% dplyr::filter(STUSPS != "HI") %>% dplyr::filter(STUSPS != "MP") %>% dplyr::filter(STUSPS != "PR") %>% dplyr::filter(STUSPS != "VI")
+us_states <- us_states %>% dplyr::filter(STUSPS != "AK") %>% dplyr::filter(STUSPS != "AS") %>% dplyr::filter(STUSPS != "DC") %>% dplyr::filter(STUSPS != "GU") %>% dplyr::filter(STUSPS != "HI") %>% dplyr::filter(STUSPS != "MP") %>% dplyr::filter(STUSPS != "PR") %>% dplyr::filter(STUSPS != "VI")
 
 beef_2022_bycounty$County <- tolower(beef_2022_bycounty$County)
 beef_2022_bycounty$State <- tolower(beef_2022_bycounty$State)
@@ -23,5 +23,4 @@ us_counties$STATE_NAME <-tolower(us_counties$STATE_NAME)
 
 beef_2022_map <- merge(us_counties, beef_2022_bycounty, by.x = c("STATE_NAME", "NAME"), by.y = c("State", "County"), all.x = TRUE) %>% st_as_sf()
 
-names(beef_2022_map)[names(beef_2022_map) == "STATE_NAME"] <- "STATE"
-names(beef_2022_map)[names(beef_2022_map) == "NAME"] <- "COUNTY"
+
