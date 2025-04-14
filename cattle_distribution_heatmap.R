cf8010fde0df6bae9986d58398b0732e3d4345fe
ngmthu234 <ngmthu.234@gmail.com>
2025-04-14 10:20:13 -0400

    plot heatmap higlighting outlines states with positive BSE cases

--- cattle_distribution_heatmap.R
+++ cattle_distribution_heatmap.R
@@ -27,12 +27,14 @@ beef_2022_map <- merge(us_counties, beef_2022_bycounty, by.x = c("STATE_NAME", "
 # To plot heatmap of beef distribution across the U.S. 
 ggplot() + geom_sf(data = beef_2022_map, aes(fill = `Inventory (Animal heads)`), color = "gray30", size = 1) + geom_sf(data = us_states, fill = NA, color = "black", size = 500) + coord_sf(crs = st_crs ("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96")) + scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") + labs(title = "Beef cattle population by U.S. county", fill = "Inventory (Animal heads)") + theme_void()
 
-# To add pin pointing at states with positive BSE cases (lines 32 - 38)
-
 BSE_state_name <- c("Texas", "Alabama", "California", "Florida", "South Carolina")
 
 BSE_state <- us_states %>% dplyr::filter(NAME %in% BSE_state_name) %>% mutate(bse_status = "State with positive BSE case(s)")
 
 BSE_state_centroid <- st_centroid(BSE_state)
 
+# To add pin pointing at states with positive BSE cases
 ggplot() + geom_sf(data = beef_2022_map, aes(fill = `Inventory (Animal heads)`), color = "gray30", size = 0.1) + geom_sf(data = us_states, fill = NA, color = "black", size = 5) + geom_sf(data = BSE_state_centroid, aes(color = bse_status, shape = bse_status), size = 5) + scale_color_manual(name = NULL, values = c("State with positive BSE case(s)" = "red")) + scale_shape_manual(name = NULL, values = c("State with positive BSE case(s)" = 17)) + coord_sf(crs = st_crs ("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96")) + scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") + labs(title = "Beef cattle population by U.S. county", fill = "Inventory (Animal heads)") + theme_void()
+
+# To highlight outlines of states with positive BSE cases
+ggplot() + geom_sf(data = beef_2022_map, aes(fill = `Inventory (Animal heads)`), color = "gray30", size = 0.1) + geom_sf(data = us_states, fill = NA, color = "black", size = 5) + geom_sf(data = BSE_state, aes(color = bse_status), fill = NA, linewidth = 1.5) + scale_color_manual(name = NULL, values = c("State with positive BSE case(s)" = "red")) + coord_sf(crs = st_crs ("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96")) + scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") + labs(title = "Beef cattle population by U.S. county", fill = "Inventory (Animal heads)") + theme_void()
