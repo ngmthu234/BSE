d7dbe21c8bcaafe6fa266d1df35b920558654b1b
ngmthu234 <ngmthu.234@gmail.com>
2025-04-14 10:11:22 -0400

    plot heatmap with pin pointing at states with positive BSE cases

--- cattle_distribution_heatmap.R
+++ cattle_distribution_heatmap.R
@@ -34,3 +34,5 @@ BSE_state_name <- c("Texas", "Alabama", "California", "Florida", "South Carolina
 BSE_state <- us_states %>% dplyr::filter(NAME %in% BSE_state_name) %>% mutate(bse_status = "State with positive BSE case(s)")
 
 BSE_state_centroid <- st_centroid(BSE_state)
+
+ggplot() + geom_sf(data = beef_2022_map, aes(fill = `Inventory (Animal heads)`), color = "gray30", size = 0.1) + geom_sf(data = us_states, fill = NA, color = "black", size = 5) + geom_sf(data = BSE_state_centroid, aes(color = bse_status, shape = bse_status), size = 5) + scale_color_manual(name = NULL, values = c("State with positive BSE case(s)" = "red")) + scale_shape_manual(name = NULL, values = c("State with positive BSE case(s)" = 17)) + coord_sf(crs = st_crs ("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96")) + scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") + labs(title = "Beef cattle population by U.S. county", fill = "Inventory (Animal heads)") + theme_void()
