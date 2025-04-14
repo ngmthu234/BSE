e6b4e40934f04e549267276ae792ee8f9e442de0
ngmthu234 <ngmthu.234@gmail.com>
2025-04-14 08:46:30 -0400

    Add comment directly within code

--- cattle_distribution_heatmap.R
+++ cattle_distribution_heatmap.R
@@ -1,4 +1,5 @@
 beef_2022 <- read.csv("~/Documents/BSE project/cow_inventory_beef_2022.csv")
+# Change "~/Documents/BSE project/cow_inventory_beef_2022.csv" with your actual path
 
 library(tidyverse)
 beef_2022 <- beef_2022 %>% dplyr::filter(Value != " (D)") %>% mutate(Value = gsub(",", "", Value)) %>% mutate(Value = as.numeric(Value))
@@ -23,4 +24,5 @@ us_counties$STATE_NAME <-tolower(us_counties$STATE_NAME)
 
 beef_2022_map <- merge(us_counties, beef_2022_bycounty, by.x = c("STATE_NAME", "NAME"), by.y = c("State", "County"), all.x = TRUE) %>% st_as_sf()
 
+# To plot heatmap
 ggplot() + geom_sf(data = beef_2022_map, aes(fill = `Inventory (Animal heads)`), color = "gray30", size = 1) + geom_sf(data = us_states, fill = NA, color = "black", size = 500) + coord_sf(crs = st_crs ("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96")) + scale_fill_gradient(low = "lightblue", high = "darkblue", na.value = "gray90") + labs(title = "Beef cattle population by U.S. county", fill = "Inventory (Animal heads)") + theme_void()
