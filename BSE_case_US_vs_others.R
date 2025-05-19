5666d4a9a8bbea3bc74076421159384dfceaf093
ngmthu234 <ngmthu.234@gmail.com>
2025-05-19 15:39:23 -0500

    plot stacked bar chart of BSE cases by country and type

--- BSE_case_US_vs_others.R
+++ BSE_case_US_vs_others.R
@@ -18,4 +18,9 @@ BSE_case_format <- BSE_case %>%
      pivot_longer(cols = `2003`:`2023`, names_to = "Year", values_to = "Cases")     # to pivot the data from wide to long format
 
 # Plot line chart of BSE cases from 2003 to 2023
-ggplot(format, aes(x = Year, y = Cases, color = Country, group = Country) + geom_line(linewidth = 1) + labs(title = "BSE cases in Canada, Japan, Europe, and the United States between 2003 and 2023", x = "Year", y = "Number of cases", color = "")
+ggplot(format, aes(x = Year, y = Cases, color = Country, group = Country) + geom_line(linewidth = 1) + labs(title = "BSE cases in Canada, Japan, Europe, and the United States between 2003 and 2023", x = "Year", y = "Number of cases", color = "Country")
+
+# Plot stacked bar chart of BSE cases by country and type from 2003 to 2023
+BSE_type <- read_excel(file_path, sheet = "bse-type2")
+BSE_type_format <- BSE_type %>% pivot_longer(cols = c(`C-BSE`, `H-BSE`, `L-BSE), names_to = "BSE_type", values_to = "Cases")
+ggplot(BSE_type_format, aes(fill = Country, y = Cases, x= Year)) + geom_bar(position = "stack", stat = "identity") + facet_wrap(~BSE_type, ncol = 1) + scale_x_continuous(breaks = 2003:2023) + scale_y_continuous(breaks = c(0, 2, 4, 6, 8, 10, 12, 14)) + labs(title = "BSE type in Canada, Japan, and the United States between 2003 and 2023", x = "Year", y = "Number of Cases", fill = "Country") + theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "right", panel.grid.major.x = element_blank(), panel.grid.minor = element_blank ())
